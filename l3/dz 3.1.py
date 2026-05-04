70dc3633ebbeab61464697fb36d83a1002db3bd9
havrylenko <vsembobra1@gmail.com>
2026-05-04 21:53:38 +0300

    Dz #3.1 виправлено

--- l3/dz 3.1.py
+++ l3/dz 3.1.py
@@ -19,27 +19,3 @@ elif operation == "/":
         print("Результат:", result)
 else:
     print("Помилка")
-#############3.2##############
-def last_to_first(lst):
-    if len(lst) <= 1:
-        return lst
-    return lst[-1:] + lst[:-1]
-list1 = ['i', 'l', 'l', 'e', 'l', 'H']
-list2 = [1]
-list3 = []
-print(f"Список 1: {last_to_first(list1)}")
-print(f"Список 2: {last_to_first(list2)}")
-print(f"Список 3: {last_to_first(list3)}")
-#############3.3##############
-def split_list(input_list):
-    length = len(input_list)
-    mid = (length + 1) // 2
-    left_part = input_list[:mid]
-    right_part = input_list[mid:]
-    return [left_part], [right_part]
-list1 = [1, 3, 7, 4, 6, 9]
-list2 = [6, 4, 1]
-list3 = []
-print(f"Список 1 {list1} -> {split_list(list1)}")
-print(f"Список 2 {list2} -> {split_list(list2)}")
-print(f"Список 3 {list3} -> {split_list(list3)}")

288c9187a78c79a9828b667152670ce6f6623874
havrylenko <vsembobra1@gmail.com>
2026-05-12 17:03:44 +0300

    Доповнено калькулятор із ДЗ 3.1

---
+++ l5/dz 5.2.py
@@ -0,0 +1,26 @@
+while True:
+    first_number = float(input("Введіть перше число: "))
+    second_number = float(input("Введіть друге число: "))
+    operation = input("Введіть математичну дію (+, -, *, /): ")
+    if operation == "+":
+        result = first_number + second_number
+        print("Результат:", result)
+    elif operation == "-":
+        result = first_number - second_number
+        print("Результат:", result)
+    elif operation == "*":
+        result = first_number * second_number
+        print("Результат:", result)
+    elif operation == "/":
+        if second_number == 0:
+            print("Така можливість є лише у Геннадія Кернеса, якщо ви не Кернес то ділення на нуль неможливе))))")
+        else:
+            result = first_number / second_number
+            print("Результат:", result)
+    else:
+        print("Помилка")
+
+    answer = input("Для продовження введіть yes: ")
+    if answer != "yes" and answer != "y":
+      print ("Кінець")
+      break
